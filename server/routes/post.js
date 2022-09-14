05c816efb076702abe746bbad4a1d804f21511c5
chanho <chjo1245@gmail.com>
2022-09-14 17:47:46 +0900

    Add: thumbnail

--- server/routes/post.js
+++ server/routes/post.js
@@ -173,7 +173,7 @@ router.delete('/:postId', isLoggedIn, async (req, res, next) => {
 router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {
   // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.location));
+  res.json(req.files.map((v) => v.location.replace(/\/original\//, '/thumb/')));
 });
 
 router.get('/:postId', async (req, res, next) => {
